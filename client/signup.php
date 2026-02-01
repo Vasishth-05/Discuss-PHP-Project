2866847bf605fbf760db86a40114903bd51eb1f5
Vasishth <learnvasishth@gmail.com>
2026-02-01 15:28:04 +0530

    Add Discuss PHP Project

---
+++ client/signup.php
@@ -0,0 +1,22 @@
+<div class="container">
+    <h1 class="heading">Signup</h1>
+    <form>
+        <div class="sm-3 col-6 offset-sm-3 margin-bottom-15">
+            <label for="username" class="form-label">Username :</label>
+            <input type="text" class="form-control" id="username" aria-describedby="emailHelp" placeholder="Enter username">
+        </div>
+        <div class="sm-3 col-6 offset-sm-3 margin-bottom-15">
+            <label for="Email" class="form-label">Email :</label>
+            <input type="text" class="form-control" id="Email" aria-describedby="emailHelp" placeholder="Enter email">
+        </div>
+        <div class="mb-3 sm-3 col-6 offset-sm-3 margin-bottom-15">
+            <label for="exampleInputPassword1" class="form-label">Password :</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter your password">
+        </div>
+        <div class="mb-3 form-check offset-sm-3">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary offset-sm-3">Submit</button>
+    </form>
+</div>
