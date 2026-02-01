1a4a416765e620622c5ca9f3c78641d8ba06c821
Vasishth <learnvasishth@gmail.com>
2026-02-01 20:56:07 +0530

    Add server requests endpoint

--- client/login.php
+++ client/login.php
@@ -9,10 +9,6 @@
             <label for="exampleInputPassword1" class="form-label">Password :</label>
             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter your password">
         </div>
-        <div class="mb-3 form-check offset-sm-3">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary offset-sm-3">Submit</button>
+        <button type="submit" class="btn btn-primary offset-sm-3">Login</button>
     </form>
 </div>
--- client/signup.php
+++ client/signup.php
@@ -1,22 +1,18 @@
 <div class="container">
     <h1 class="heading">Signup</h1>
-    <form>
+    <form method="post" action="./server/requests.php">
         <div class="sm-3 col-6 offset-sm-3 margin-bottom-15">
             <label for="username" class="form-label">Username :</label>
-            <input type="text" class="form-control" id="username" aria-describedby="emailHelp" placeholder="Enter username">
+            <input type="text" name="username" class="form-control" id="username" aria-describedby="emailHelp" placeholder="Enter username">
         </div>
         <div class="sm-3 col-6 offset-sm-3 margin-bottom-15">
             <label for="Email" class="form-label">Email :</label>
-            <input type="text" class="form-control" id="Email" aria-describedby="emailHelp" placeholder="Enter email">
+            <input type="text" name="email" class="form-control" id="Email" aria-describedby="emailHelp" placeholder="Enter email">
         </div>
         <div class="mb-3 sm-3 col-6 offset-sm-3 margin-bottom-15">
             <label for="exampleInputPassword1" class="form-label">Password :</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter your password">
+            <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Enter your password">
         </div>
-        <div class="mb-3 form-check offset-sm-3">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary offset-sm-3">Submit</button>
+        <button type="submit" name="signup" class="btn btn-primary offset-sm-3">Signup</button>
     </form>
 </div>
