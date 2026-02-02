912d253b1e08a9853b024338c8fe37b67a58ac9b
Vasishth <learnvasishth@gmail.com>
2026-02-02 20:15:27 +0530

    add request endpoint

--- server/requests.php
+++ server/requests.php
@@ -1,12 +1,25 @@
 <?php
     
+    session_start();
+
     include("../common/db.php");
 
     if(isset($_POST['signup'])){
-        echo "User name is ".$_POST['username']."<br/>";
-        echo "User email is ".$_POST['email']."<br/>";
-        echo "User password is ".$_POST['password']."<br/>";
-
         $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $user = $conn -> prepare("Insert into 
+        `users`(`id`,`username`,`email`,`password`) 
+        values(NULL,'$username','$email','$password')");
+
+        $result = $user->execute();
+
+        if($result){
+            echo " new user registered";
+            $_SESSION["user"] = ["username"=>$username,"email"=>$email];
+        } else {
+            echo "new user not registered";
+        }
     }
 ?>
