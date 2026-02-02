fe8f625371025d79cb1c28bc3b8487e3f7b62f48
Vasishth <learnvasishth@gmail.com>
2026-02-02 12:47:35 +0530

    Add database

--- server/requests.php
+++ server/requests.php
@@ -1,7 +1,12 @@
 <?php
+    
+    include("../common/db.php");
+
     if(isset($_POST['signup'])){
         echo "User name is ".$_POST['username']."<br/>";
         echo "User email is ".$_POST['email']."<br/>";
         echo "User password is ".$_POST['password']."<br/>";
+
+        $username = $_POST['username'];
     }
 ?>
